e040d512ccfe0d567e3297ac0ba8a7b8b9e1bb39
Guy Marshall <guymarshall9@gmail.com>
2022-01-01 14:36:58 +0000

    Copied example of webcam capture to learn from and experiment with

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
-print("Just to get the git repo to initialise")
+# the following is an example from "https://stackoverflow.com/questions/2601194/displaying-a-webcam-feed-using-opencv-and-python/11449901#11449901"
+
+import cv2
+
+cv2.namedWindow("preview")
+video_capture = cv2.VideoCapture(0)
+
+if video_capture.isOpened(): # try to get the first frame
+    rval, frame = video_capture.read()
+else:
+    rval = False
+
+while rval:
+    cv2.imshow("preview", frame)
+    rval, frame = video_capture.read()
+    key = cv2.waitKey(20)
+    if key == 27: # exit on ESC
+        break
+
+video_capture.release()
+cv2.destroyWindow("preview")
